c035a57165efc2f2bb06c54892524c0de71f3d34
TeoEstudio <dei.guiribalde@gmail.com>
2021-12-14 18:18:36 -0500

    Vista Subir Excel

--- resources/views/anulation/create.blade.php
+++ resources/views/anulation/create.blade.php
@@ -147,19 +147,7 @@
                 </div>
 
 
-                <div class="col-lg-12 mt-8 border p-48">
-                    <div class="row align-items-center">
-                        <div class="col-lg-12 text-center font-weight-bold">
-                            <img src="{{asset('assets/img/icono-excel.png')}}" width="100">
-                            <h2 class="mt-16">Agregar detalles de anulación</h2>
-                            <p class="mb-0">Carga Excel (.xls)*</p>
-                        </div>
-                        <div class="col-lg-12 text-center mt-20">
-                            <input type="file" class="form-control m-auto" placeholder="RUT" style="max-width:400px;">
-                            <a class="btn bg-pri border-pri text-white pl-24 pr-24 mt-sm-20 font-weight-bold mt-24" style="min-width:190px;"> Subir registros</a>
-                        </div>
-                    </div>
-                </div>
+
 
             </div>
         </div>
--- resources/views/anulation/excel.blade.php
+++ resources/views/anulation/excel.blade.php
@@ -2,9 +2,19 @@
 @section('title','Subir Excel')
 @section('app-content')
     <div class="main-content p-48 p-sm-16 bg-white mt-sm-16">
-        <div class="text-left">
-            <h2 class="text-pri">Subir Excel</h2>
-        </div>
+    <div class="col-lg-12 mt-8 border p-48">
+                    <div class="row align-items-center">
+                        <div class="col-lg-12 text-center font-weight-bold">
+                            <img src="{{asset('assets/img/icono-excel.png')}}" width="100">
+                            <h2 class="mt-16">Agregar detalles de anulación XXX</h2>
+                            <p class="mb-0">Carga Excel (.xls)*</p>
+                        </div>
+                        <div class="col-lg-12 text-center mt-20">
+                            <input type="file" class="form-control m-auto" placeholder="RUT" style="max-width:400px;">
+                            <a class="btn bg-pri border-pri text-white pl-24 pr-24 mt-sm-20 font-weight-bold mt-24" style="min-width:190px;"> Subir registros</a>
+                        </div>
+                    </div>
+                </div>
     </div>
 
 
